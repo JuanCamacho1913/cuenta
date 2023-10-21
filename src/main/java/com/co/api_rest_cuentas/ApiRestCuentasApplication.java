d680b1422c2c8254ef15a3308d0a946005f427bb
¨juan <jecb1913@gmail.com>
2023-10-21 14:35:40 -0500

    Inicio de proyecto REST API, Cuentas

---
+++ src/main/java/com/co/api_rest_cuentas/ApiRestCuentasApplication.java
@@ -0,0 +1,13 @@
+package com.co.api_rest_cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCuentasApplication.class, args);
+	}
+
+}
